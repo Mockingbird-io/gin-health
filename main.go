4b28d2e46939b6dfb94327636a6c044704dc2361
lazy <497196693@qq.com>
2020-09-22 19:57:20 +0800

    code

--- main.go
+++ main.go
@@ -1,20 +1,29 @@
 package main
+
 import "fmt"
 
-type struct1 struct {
-	i1  int
-	f1  float32
-	str string
+type TwoInit struct {
+	a int
+	b int
 }
 
 func main() {
-	ms := new(struct1)
-	ms.i1 = 10
-	ms.f1 = 15.5
-	ms.str= "Chris"
+	two1 := new(TwoInit)
+	two1.a = 2
+	two1.b = 3
+	fmt.Printf("The sum is: %d\n", two1.AddThem())
+	fmt.Printf("Add them to the param: %d\n", two1.AddToParam(20))
 
-	fmt.Printf("The int is: %d\n", ms.i1)
-	fmt.Printf("The float is: %f\n", ms.f1)
-	fmt.Printf("The string is: %s\n", ms.str)
-	fmt.Println(ms)
+	two2 := TwoInit{
+		a: 20,
+		b: 30,
+	}
+	fmt.Printf("The sum is: %d\n", two2.AddToParam(1))
+}
+
+func (tn *TwoInit) AddThem() int {
+	return tn.a + tn.b
+}
+func (tn *TwoInit) AddToParam(param int) int {
+	return tn.a + tn.b + param
 }
