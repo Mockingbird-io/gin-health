bedc7b38a21169898500d270f257ddd8c699c06e
lazy <497196693@qq.com>
2020-11-23 10:58:37 +0800

    gins_init

--- main.go
+++ main.go
@@ -1,29 +1,13 @@
 package main
 
-import "fmt"
-
-type TwoInit struct {
-	a int
-	b int
-}
+import "github.com/gin-gonic/gin"
 
 func main() {
-	two1 := new(TwoInit)
-	two1.a = 2
-	two1.b = 3
-	fmt.Printf("The sum is: %d\n", two1.AddThem())
-	fmt.Printf("Add them to the param: %d\n", two1.AddToParam(20))
-
-	two2 := TwoInit{
-		a: 20,
-		b: 30,
-	}
-	fmt.Printf("The sum is: %d\n", two2.AddToParam(1))
-}
-
-func (tn *TwoInit) AddThem() int {
-	return tn.a + tn.b
-}
-func (tn *TwoInit) AddToParam(param int) int {
-	return tn.a + tn.b + param
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	_ = r.Run() // listen and serve on 0.0.0.0:8080
 }
